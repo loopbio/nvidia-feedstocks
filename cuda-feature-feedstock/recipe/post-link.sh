ece31b2ecfafb6123008371720b179e6d9b7fe38
santi <sdvillal@gmail.com>
2017-03-05 18:00:44 +0100

    cuda feature package

---
+++ cuda-feature-feedstock/recipe/post-link.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# See: https://conda.io/docs/building/build-scripts.html
+
+# We probably could be clever here and only tell the user if
+# (the correct version) of CUDA is not there.
+
+echo "
+###############################
+#
+# The CUDA runtime should be installed in this machine.
+# If that is not the case, you can either:
+#
+#   - Use your system package manager.
+#
+#   - Use any suitable package from:
+#     http://www.anaconda.org
+#
+#   - Install it from:
+#     https://developer.nvidia.com/cuda-zone
+#
+###############################
+" > ${PREFIX}/.messages.txt
